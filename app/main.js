2cd57385f5d47f0002c654fc79e8eb1842d37ac3
Jacek Kolasa <jacek.kolasa@gmail.com>
2016-04-19 22:26:35 +0200

    plug in firebase and bind it to player's volume

--- app/main.js
+++ app/main.js
@@ -1,14 +1,18 @@
 import Cycle from '@cycle/core'
 import {div, makeDOMDriver} from '@cycle/dom'
 import {Observable} from 'rx'
+import Firebase from 'firebase'
+import {makeFirebaseDriver} from 'cycle-firebase'
 
 import Player from './player'
 
 function main(src) {
   const playerSinks = Player({
-    DOM: src.DOM
+    DOM: src.DOM,
+    firebase: src.firebase
   })
   const playerVTree$ = playerSinks.DOM
+  const playerFirebase$ = playerSinks.firebase
 
   return {
     DOM: Observable.combineLatest(
@@ -16,10 +20,17 @@ function main(src) {
         div('.main', [
           player
         ])
+    ),
+    firebase: Observable.combineLatest(
+      playerFirebase$, (playerFirebase) => {
+        return playerFirebase;
+      }
     )
   }
 }
 
+const ref = new Firebase("https://remote-noisemaker.firebaseIO.com")
 Cycle.run(main, {
+  firebase: makeFirebaseDriver(ref),
   DOM: makeDOMDriver('#app')
 })
--- app/player/index.js
+++ app/player/index.js
@@ -8,19 +8,46 @@ export default function main(src) {
   const minusClick$ = clickFrom('.minus-button').map(-1)
   const plusClick$ = clickFrom('.plus-button').map(1)
 
-  const volume$ = Observable.merge(minusClick$, plusClick$)
-    .startWith(5)
-    .scan((acc, x) => acc + x)
+  const volumeRelativeChange$ = Observable
+    .merge(minusClick$, plusClick$)
+    .startWith(0)
+
+  const volumeFromFirebase$ = src.firebase.get('volume')
+    .share()
+
+  /**
+   * volumeFromFirebase$:
+   * --5-------------------------2---
+   *
+   * volumeRelativeChange$:
+   * ------ -1 -------+1-- -1 -------
+   *
+   * volume$:
+   * --5-----4---------5----4----2---
+   */
+  const volume$ = volumeFromFirebase$
+    .map((volume) => volumeRelativeChange$
+      .scan((acc, change) => acc + change, volume)
+    ).switch()
+    .distinctUntilChanged()
+    .share()
+
+  const volumeToDOM$ = volume$.startWith(5)
+  const volumeChange$ = volume$.filter((x, i) => i !== 0)
 
   return {
     DOM: Rx.Observable.combineLatest(
-      volume$, (volume) =>
+      volumeToDOM$, (volume) =>
         div([
           span(`current volue: ${volume}`),
           br(),
           button('.minus-button', '-'),
           button('.plus-button', '+')
         ])
-      )
+    ),
+    firebase: volumeChange$.map((volume) => ({
+        volume
+      })
+    )
   }
 }
